0563675cfd0b906f1460e1749782d8dca19a3fca
cyrisek <cyrisek07@gmail.com>
2023-07-06 21:07:48 +0100

    yt clone

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+export const BASE_URL = "https://youtube-v31.p.rapidapi.com";
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    "X-RapidAPI-Key": "5d5689a9d4msh1faec72651bfb86p18f432jsn95ca21d2251d",
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
