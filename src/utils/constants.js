4a675aad3f7269470fedef0888a20907ecd8899a
cyrisek <cyrisek07@gmail.com>
2023-07-13 21:33:21 +0100

    color theme update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,15 +11,16 @@ import GraphicEqIcon from "@mui/icons-material/GraphicEq";
 import TheaterComedyIcon from "@mui/icons-material/TheaterComedy";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
 import DeveloperModeIcon from "@mui/icons-material/DeveloperMode";
+import MyImage from "./img/ytBlue.png";
 
-export const logo = "https://i.ibb.co/s9Qys2j/logo.png";
+export const logo = MyImage;
 
 export const categories = [
   { name: "New", icon: <HomeIcon /> },
-  { name: "JS Mastery", icon: <CodeIcon /> },
+  { name: "CS50", icon: <CodeIcon /> },
   { name: "Coding", icon: <CodeIcon /> },
   { name: "ReactJS", icon: <CodeIcon /> },
-  { name: "NextJS", icon: <CodeIcon /> },
+  { name: "Django Python", icon: <CodeIcon /> },
   { name: "Music", icon: <MusicNoteIcon /> },
   { name: "Education", icon: <SchoolIcon /> },
   { name: "Podcast", icon: <GraphicEqIcon /> },
